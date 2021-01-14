d981fbb94e7264374c10ec640cae70cfb4467f03
Zoe Kirsh <zoekirsh@gmail.com>
2021-01-14 14:18:48 -0800

    all passing

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main')
-  books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBooks();
+});
